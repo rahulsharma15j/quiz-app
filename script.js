2622ceae76c12a4265db18e2958bc756101d910b
Rahul Sharma <rahulsharma15@gmail.com>
2023-07-21 20:37:18 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,48 @@
+//questions list
+let questions = [
+  {
+    question: "1. The full form of CSS is:",
+    answers: [
+      { option: "Cascading Style Sheets", isTrue: true },
+      { option: "Coloured Special Sheets", isTrue: false },
+      { option: "Color and Style Sheets", isTrue: false },
+      { option: "None of the above", isTrue: false },
+    ],
+  },
+  {
+    question: "2. How can we change the background color of an element?",
+    answers: [
+      { option: "background-color", isTrue: true },
+      { option: "color", isTrue: false },
+      { option: "Both A and B", isTrue: false },
+      { option: "None of the above", isTrue: false },
+    ],
+  },
+  {
+    question: "3. How can we change the text color of an element?",
+    answers: [
+      { option: "background-color", isTrue: false },
+      { option: "color", isTrue: true },
+      { option: "Both A and B", isTrue: false },
+      { option: "None of the above", isTrue: false },
+    ],
+  },
+  {
+    question: "4. In how many ways can CSS be written in?",
+    answers: [
+      { option: "1", isTrue: false },
+      { option: "2", isTrue: false },
+      { option: "3", isTrue: true },
+      { option: "4", isTrue: false },
+    ],
+  },
+  {
+    question: "5. Can negative values be allowed in padding property?",
+    answers: [
+      { option: "Yes", isTrue: true },
+      { option: "No", isTrue: false },
+      { option: "Depends on property", isTrue: false },
+      { option: "None of the above", isTrue: false },
+    ],
+  },
+];
